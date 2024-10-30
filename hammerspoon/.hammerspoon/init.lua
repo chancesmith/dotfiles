32e6473d03bedf22c40dab0f6e306e9da4794370
Chance Smith <chance.smith@lifeway.com>
2024-10-30 13:24:58 -0500

    : remove window management

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -106,27 +106,4 @@ hs.hotkey.bind(hyper, "e", function ()
   hs.eventtap.keyStroke({"ctrl", "cmd"}, "space")
 end)
 
--- Window Manager
-spoon.SpoonInstall:andUse("MiroWindowsManager")
-hs.window.animationDuration = 0.3
-spoon.MiroWindowsManager:bindHotkeys({
-  up = {hyper, "up"},
-  right = {hyper, "right"},
-  down = {hyper, "down"},
-  left = {hyper, "left"},
-  fullscreen = {hyper, "f"}
-})
-
-function moveWindowToDisplay(d)
-  return function()
-    local displays = hs.screen.allScreens()
-    local win = hs.window.focusedWindow()
-    win:moveToScreen(displays[d], false, true)
-  end
-end
-
-hs.hotkey.bind(hyper, "1", moveWindowToDisplay(1))
-hs.hotkey.bind(hyper, "2", moveWindowToDisplay(2))
-hs.hotkey.bind(hyper, "3", moveWindowToDisplay(3))
-
 hs.notify.new({title='Hammerspoon', informativeText='Everything is hawt 🔥'}):send()
