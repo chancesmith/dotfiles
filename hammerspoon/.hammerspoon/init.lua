59fb9d8331c9827863e761b4e9cd5c6657e0e07c
Chance Smith <chance.smith@lifeway.com>
2026-01-30 12:26:42 -0600

    update hammerspoon links

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -50,11 +50,15 @@ local mehQuickLinks = {
   { key = "v", url = "https://vault.prod.lifeway.com/ui/vault/secrets/kv/show/digital-exp-dce/test-accounts/int/lifeway-one" },
   { key = "w", url = "https://outlook.office365.com/calendar/view/workweek" }, -- calendar (moved off meh+c)
   { key = "m", url = "https://mail.google.com/mail/u/0/" },
-  { key = "j", url = "https://lifeway.atlassian.net/jira/software/c/projects/DCD/boards/489?assignee=64170c71af3b93d8ecf2bf15" },
+  { key = "j", url = "https://lifeway.atlassian.net/jira/software/c/projects/DCD/boards/489" }, -- Jira Board
+  { key = "t", url = "https://lifeway.atlassian.net/jira/software/c/projects/DCD/boards/489/timeline"}, -- Jira Timeline
   { key = "s", url = "https://lifeway-christian-resources.sentry.io/issues/?environment=production&project=4503902988730368&statsPeriod=14d" }, -- Sentry
   { key = "p", url = "https://www.perplexity.ai/" },
   { key = "k", url = "https://grok.com/" },
-  { key = "l", url = "https://claude.ai/" }
+  { key = "l", url = "https://claude.ai/" },
+  { key = "u", url = "https://www.loom.com/settings/users"},
+  { key = "o", url = "https://lifewaychristianresources-my.sharepoint.com/my?csf=1&web=1&e=4L4iF4&id=%2Fpersonal%2Fchance%5Fsmith%5Flifeway%5Fcom%2FDocuments%2FSDM&FolderCTID=0x0120001CC2D9AE130C0C468A19E85185A451BB"}, -- SDM Folder in OneDrive
+  { key = "s", url = "https://lifewaychristianresources-my.sharepoint.com/:x:/r/personal/chance_smith_lifeway_com/_layouts/15/Doc.aspx?sourcedoc=%7B7723327B-4E1F-4860-9ECB-A2E8A566656F%7D&file=Chance%20Smith%20employees.xlsx&action=default&mobileredirect=true"} -- Staff sheet
 }
 
 for _, link in ipairs(mehQuickLinks) do
@@ -66,7 +70,7 @@ end
 -- Secrets-backed Zoom links stay explicit
 hs.hotkey.bind(meh, "1", function ()
   local secretURL = secrets.ZOOM_LIFEWAY_ONE
-  
+
   if secretURL and secretURL ~= "" then
       hs.urlevent.openURL(secretURL)
   else
@@ -75,8 +79,8 @@ hs.hotkey.bind(meh, "1", function ()
 end)
 
 hs.hotkey.bind(meh, "2", function ()
-  local secretURL = secrets.ZOOM_LIFEWAY_ONE_READER 
-  
+  local secretURL = secrets.ZOOM_LIFEWAY_ONE_READER
+
   if secretURL and secretURL ~= "" then
       hs.urlevent.openURL(secretURL)
   else
